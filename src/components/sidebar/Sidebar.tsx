4fb35aca56540719891905f224e0c3bed20474d5
vviktorgraphic <veres.viktor.graphic@gmail.com>
2026-07-01 22:09:59 +0200

    feat(sidebar): add filter controls

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,10 @@
 import {
     Box,
+    Button,
     Divider,
+    MenuItem,
+    Stack,
+    TextField,
     Typography
 } from "@mui/material";
 
@@ -13,20 +17,101 @@ export default function Sidebar() {
                 display: "flex",
                 flexDirection: "column",
                 height: "100%",
-                p: 2
+                p: 2,
+                gap: 2,
+                overflowY: "auto"
             }}
         >
 
             <Typography
                 variant="h6"
-                gutterBottom
             >
-
                 Szűrők
-
             </Typography>
 
             <Divider />
+
+            <TextField
+                size="small"
+                label="Gyorskereső"
+                fullWidth
+            />
+
+            <TextField
+                select
+                size="small"
+                label="Szervizcsapat"
+                fullWidth
+                defaultValue=""
+            >
+                <MenuItem value="">
+                    Minden csapat
+                </MenuItem>
+
+                <MenuItem value="HT">
+                    Help-Trak Kft.
+                </MenuItem>
+
+                <MenuItem value="TP">
+                    Turcsányi Péter
+                </MenuItem>
+            </TextField>
+
+            <TextField
+                size="small"
+                label="Munkalap"
+                placeholder="Pl.: HT-0008218"
+                fullWidth
+            />
+
+            <TextField
+                size="small"
+                label="Partner"
+                fullWidth
+            />
+
+            <TextField
+                size="small"
+                label="Technikus"
+                fullWidth
+            />
+
+            <TextField
+                size="small"
+                label="Géptípus"
+                fullWidth
+            />
+
+            <TextField
+                size="small"
+                label="Gyári szám"
+                fullWidth
+            />
+
+            <Stack spacing={2}>
+
+                <TextField
+                    size="small"
+                    label="Dátumtól"
+                    placeholder="ÉÉÉÉ.HH.NN"
+                    fullWidth
+                />
+
+                <TextField
+                    size="small"
+                    label="Dátumig"
+                    placeholder="ÉÉÉÉ.HH.NN"
+                    fullWidth
+                />
+
+            </Stack>
+
+            <Button
+                variant="outlined"
+                fullWidth
+            >
+                Szűrők törlése
+            </Button>
 
         </Box>
 
